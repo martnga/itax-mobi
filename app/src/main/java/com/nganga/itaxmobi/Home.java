203c0fa764c816adcfff059269604c2573b657b4
nganga <mtnjoroge@gmail.com>
2015-09-11 16:19:51 +0300

    Added listItems background gradient'

--- app/src/main/java/com/nganga/itaxmobi/Home.java
+++ app/src/main/java/com/nganga/itaxmobi/Home.java
@@ -2,16 +2,22 @@ package com.nganga.itaxmobi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 /**
  * Created by nganga on 9/11/15.
  */
 public class Home extends Activity {
 
+    RecyclerView registration_rv;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+
 
     }
 }
