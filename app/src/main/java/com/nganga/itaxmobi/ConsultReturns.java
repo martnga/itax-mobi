2d2a0c8dc5e985de30c622075ab7043d502124ed
nganga <mtnjoroge@gmail.com>
2015-09-17 02:32:52 +0300

    Drag and dropped files

--- app/src/main/java/com/nganga/itaxmobi/ConsultReturns.java
+++
@@ -1,9 +0,0 @@
-package com.nganga.itaxmobi;
-
-import android.app.Activity;
-
-/**
- * Created by nganga on 9/15/15.
- */
-public class ConsultReturns extends Activity {
-}
--- app/src/main/java/com/nganga/itaxmobi/E_ReturnsMenu.java
+++
@@ -1,9 +0,0 @@
-package com.nganga.itaxmobi;
-
-import android.app.Activity;
-
-/**
- * Created by nganga on 9/14/15.
- */
-public class E_ReturnsMenu extends Activity{
-}
