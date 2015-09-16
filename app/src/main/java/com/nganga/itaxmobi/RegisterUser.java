523c01547ca4ecab83d9acbe7b1e5c9409269e9f
nganga <mtnjoroge@gmail.com>
2015-09-16 10:49:36 +0300

    Associated registerUser to an xml

--- app/src/main/java/com/nganga/itaxmobi/RegisterUser.java
+++ app/src/main/java/com/nganga/itaxmobi/RegisterUser.java
@@ -1,10 +1,20 @@
 package com.nganga.itaxmobi;
 
 import android.app.Activity;
+import android.os.Bundle;
+
 
 
 /**
  * Created by nganga on 9/9/15.
  */
 public class RegisterUser extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_user);
+
+    }
 }
