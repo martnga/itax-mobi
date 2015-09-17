6115054675d0adcfbd207502757e47daf57b2d4e
nganga <mtnjoroge@gmail.com>
2015-09-17 03:26:51 +0300

    Added E_payments menu options

---
+++ app/src/main/java/com/nganga/itaxmobi/E_PaymentsMenu.java
@@ -0,0 +1,18 @@
+package com.nganga.itaxmobi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by nganga on 9/17/15.
+ */
+public class E_PaymentsMenu  extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.e_payments_menu);
+
+
+    }
+}
