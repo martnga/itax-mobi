c49a24e9f47f6af3978ff89206b63db934603d11
nganga <mtnjoroge@gmail.com>
2015-09-17 03:12:31 +0300

    Linked the home menu options to xml

--- app/src/main/java/com/nganga/itaxmobi/E_ReturnsMenu.java
+++ app/src/main/java/com/nganga/itaxmobi/E_ReturnsMenu.java
@@ -1,9 +1,18 @@
 package com.nganga.itaxmobi;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by nganga on 9/14/15.
  */
 public class E_ReturnsMenu extends Activity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.e_returns_menu);
+
+
+    }
 }
